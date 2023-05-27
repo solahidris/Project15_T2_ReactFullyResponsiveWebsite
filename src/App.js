d8be86906da18676fb1b10108d088215e72441ab
solahidris <emailsolah@gmail.com>
2023-05-27 17:41:11 +0800

    added icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
-import AppDescription from "./components/AppDescription";
+// import AppDescription from "./components/AppDescription";
+import { FaTiktok, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+//https://react-icons.github.io/react-icons (icons website)
 
 function App() {
   // Google Map API Key
@@ -23,9 +26,14 @@ function App() {
           <p className="self-center text-2xl font-mono text-black font-bold tracking-widest">
             coffee and kicks
           </p>
-          <p className="self-center text-xs font-mono text-black tracking-widest">
+          <div className="flex mt-[0.1rem]">
+          <p className="grow self-center text-xs font-mono text-black tracking-widest">
             caffeine near me
           </p>
+          <p className="mr-2 scale-75"><FaTiktok /></p>
+          <p className="mr-2 scale-75"><FaTwitter /></p>
+          <p className="scale-75"><FaInstagram /></p>
+          </div>
         </div>
       </div>
 
@@ -77,6 +85,8 @@ function App() {
             <p className="text-white font-mono text-xs text-center tracking-wide">
               📍 Hirup Cafe Seksyen 9
             </p>
+            <p className="mt-2 text-white font-mono text-[10px] text-center tracking-wide">44, Jalan Tengku Ampuan Zabedah A 9/A, Seksyen 9, 40100 Shah Alam, Selangor
+            </p>
           </div>
       </div>
 
@@ -93,7 +103,8 @@ function App() {
             {/* americano */}
             <div className="bg-gray-300/60 rounded-3xl grid grid-rows-3 grid-flow-col h-[18rem] lg:h-[30rem] transition ease-in-out hover:-translate-center-1 hover:scale-105 duration-500">
               <img src="https://globalassets.starbucks.com/assets/f12bc8af498d45ed92c5d6f1dac64062.jpg?" className="rounded-t-3xl row-end-1 row-span-2"/>
-              <p className="text-center row-start-2 row-span-1 row-end-3 text-xs font-mono">americano</p>
+              <p className="text-end mt-[-20px] mr-[8px] text-gray-200/90 text-[0.6rem] z-20 font-mono">RM 7</p>
+              <p className="text-center row-start-2 row-span-1 row-end-3 text-xs font-mono">Americano</p>
               <div className="flex justify-center row-start-4 row-end-5 pb-4">
                 <button className="px-[0.6rem] py-1 text-white rounded-full bg-emerald-900 hover:bg-gradient-to-r from-orange-500 to-red-600 transition ease-in-out hover:-translate-center-1 hover:scale-110 duration-500">H</button>
                 <button className="px-[0.6rem] py-1 text-white rounded-full ml-2 bg-emerald-900 hover:bg-gradient-to-r from-cyan-500 to-blue-500 transition ease-in-out hover:-translate-center-1 hover:scale-110 duration-500">C</button>
@@ -103,7 +114,8 @@ function App() {
             {/* latte  */}
             <div className="bg-gray-300/60 rounded-3xl grid grid-rows-3 grid-flow-col h-[18rem] lg:h-[30rem] transition ease-in-out hover:-translate-center-1 hover:scale-105 duration-500">
               <img src="https://globalassets.starbucks.com/assets/b635f407bbcd49e7b8dd9119ce33f76e.jpg?" className="rounded-t-3xl row-end-1 row-span-2"/>
-              <p className="text-center row-start-2 row-span-1 row-end-3 text-xs font-mono">latte</p>
+              <p className="text-end mt-[-20px] mr-[8px] text-gray-200/90 text-[0.6rem] z-20 font-mono">RM 10</p>
+              <p className="text-center row-start-2 row-span-1 row-end-3 text-xs font-mono">Latte</p>
               <div className="flex justify-center row-start-4 row-end-5 pb-4">
                 <button className="px-[0.6rem] py-1 text-white rounded-full bg-emerald-900 hover:bg-gradient-to-r from-orange-500 to-red-600 transition ease-in-out hover:-translate-center-1 hover:scale-110 duration-500">H</button>
                 <button className="px-[0.6rem] py-1 text-white rounded-full ml-2 bg-emerald-900 hover:bg-gradient-to-r from-cyan-500 to-blue-500 transition ease-in-out hover:-translate-center-1 hover:scale-110 duration-500">C</button>
@@ -113,7 +125,8 @@ function App() {
             {/* mocha */}
             <div className="bg-gray-300/60 rounded-3xl grid grid-rows-3 grid-flow-col h-[18rem] lg:h-[30rem] transition ease-in-out hover:-translate-center-1 hover:scale-105 duration-500">
               <img src="https://globalassets.starbucks.com/assets/915736da018842e788147f7eab73db73.jpg?" className="rounded-t-3xl row-end-1 row-span-2"/>
-              <p className="text-center row-start-2 row-span-1 row-end-3 text-xs font-mono">mocha</p>
+              <p className="text-end mt-[-20px] mr-[8px] text-gray-200/90 text-[0.6rem] z-20 font-mono">RM 11</p>
+              <p className="text-center row-start-2 row-span-1 row-end-3 text-xs font-mono">Mocha</p>
               <div className="flex justify-center row-start-4 row-end-5 pb-4">
                 <button className="px-[0.6rem] py-1 text-white rounded-full bg-emerald-900 hover:bg-gradient-to-r from-orange-500 to-red-600 transition ease-in-out hover:-translate-center-1 hover:scale-110 duration-500">H</button>
                 <button className="px-[0.6rem] py-1 text-white rounded-full ml-2 bg-emerald-900 hover:bg-gradient-to-r from-cyan-500 to-blue-500 transition ease-in-out hover:-translate-center-1 hover:scale-110 duration-500">C</button>
@@ -123,7 +136,8 @@ function App() {
             {/* caramel macchiato */}
             <div className="bg-gray-300/60 rounded-3xl grid grid-rows-3 grid-flow-col h-[18rem] lg:h-[30rem] transition ease-in-out hover:-translate-center-1 hover:scale-105 duration-500">
               <img src="https://globalassets.starbucks.com/assets/58db701349cb48738069e8c912e2b3ac.jpg?" className="rounded-t-3xl row-end-1 row-span-2"/>
-              <p className="text-center row-start-2 row-span-1 row-end-3 text-xs font-mono">caramel macchiato</p>
+              <p className="text-end mt-[-20px] mr-[8px] text-gray-200/90 text-[0.6rem] z-20 font-mono">RM 12</p>
+              <p className="text-center row-start-2 row-span-1 row-end-3 text-xs font-mono">Caramel Macchiato</p>
               <div className="flex justify-center row-start-4 row-end-5 pb-4">
                 <button className="px-[0.6rem] py-1 text-white rounded-full bg-emerald-900 hover:bg-gradient-to-r from-orange-500 to-red-600 transition ease-in-out hover:-translate-center-1 hover:scale-110 duration-500">H</button>
                 <button className="px-[0.6rem] py-1 text-white rounded-full ml-2 bg-emerald-900 hover:bg-gradient-to-r from-cyan-500 to-blue-500 transition ease-in-out hover:-translate-center-1 hover:scale-110 duration-500">C</button>
@@ -146,6 +160,19 @@ function App() {
       <div className="bg-white py-20">
          {/* Location title */}
         <p className="text-center mt-[-2.2rem] mb-10 font-mono tracking-widest text-lg"> [ socials ] </p>
+        <div className="flex justify-center gap-x-[5rem] bg-sky-300 h-[5rem]">
+          <div>
+            <p className="bg-red-200 mr-2 h-full"><FaTiktok /></p>
+          </div>
+          <div>
+            <p className="bg-red-400 mr-2 h-screen"><FaTwitter /></p>
+          </div>
+          <div>
+            <p className="bg-red-200"><FaInstagram /></p>
+          </div>
+        </div>
+        contact us formul
+        <p>label</p><input type></input>
       </div>
 
 
