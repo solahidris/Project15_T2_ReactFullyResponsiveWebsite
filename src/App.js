ac4546c68aeee6c24cfa652663347eaf67161ad0
solahidris <emailsolah@gmail.com>
2023-05-28 16:45:45 +0800

    try fix 5

--- src/App.js
+++ src/App.js
@@ -27,27 +27,26 @@ function App() {
       {/* <AppDescription /> */}
 
 {/* Website Header */}
-      <div className="flex justify-center bg-white mx-10 pr-5 transition ease-in-out hover:-translate-center-1 hover:scale-105 duration-500">
+<div className="flex justify-center bg-white rounded-3xl mx-10 pr-2 lg:mx-[20rem] transition ease-in-out hover:-translate-center-1 hover:scale-105 duration-500">
         <img
-          className="h-[7rem]"
+          className="h-[7rem] rounded-3xl"
           src={
             "https://cdn.pentas.io/next-s3-uploads/55a71fe2-14a2-4544-a912-030d47d36bb7/IMG_R_0008.JPG"
           }
           alt="logoTitle"
         />
-        <div className="self-center">
-          <p className="self-center text-xl font-mono text-black font-bold tracking-widest">
-            coffee and kicks
-          </p>
-          <div className="flex mt-[0.1rem]">
-          <p className="grow self-center text-xs font-mono tracking-widest">
-            caffeine near me
-          </p>
-          <p className="mr-2 scale-75"><FaTiktok /></p>
-          <p className="mr-2 scale-75"><FaTwitter /></p>
-          <p className="scale-75"><FaInstagram /></p>
+          <div className="self-center">
+            <p className="self-center text-lg font-mono text-black font-bold tracking-widest">
+              coffee and kicks
+            </p>
+            <div className="flex mt-[0.1rem]">
+            <p className="grow self-center text-xs font-mono tracking-widest">
+              caffeine near me
+            </p>
+            <p className="mr-2 scale-75"><FaTiktok /></p>
+            <p className="scale-75"><FaInstagram /></p>
+            </div>
           </div>
-        </div>
       </div>
 
 {/* Navbar Header */}
@@ -354,33 +353,33 @@ function App() {
       </div>
 {/* just adding a footer for now - need to adjust later */}
 
-<div className="bg-emerald-950/90 pt-10 pb-20">
+    <div className="bg-emerald-950/90 pt-10 pb-20">
       {/* <AppDescription /> */}
 
-{/* Website Header */}
+      {/* Website Header */}
       <div className="flex justify-center bg-white rounded-3xl mx-10 pr-2 lg:mx-[20rem] transition ease-in-out hover:-translate-center-1 hover:scale-105 duration-500">
         <img
-          className="h-[7rem]"
+          className="h-[7rem] rounded-3xl"
           src={
             "https://cdn.pentas.io/next-s3-uploads/55a71fe2-14a2-4544-a912-030d47d36bb7/IMG_R_0008.JPG"
           }
           alt="logoTitle"
         />
-        <div className="self-center">
-          <p className="self-center text-lg font-mono text-black font-bold tracking-widest">
-            coffee and kicks
-          </p>
-          <div className="flex mt-[0.1rem]">
-          <p className="grow self-center text-xs font-mono tracking-widest">
-            caffeine near me
-          </p>
-          <p className="mr-2 scale-75"><FaTiktok /></p>
-          <p className="mr-2 scale-75"><FaTwitter /></p>
-          <p className="scale-75"><FaInstagram /></p>
+          <div className="self-center">
+            <p className="self-center text-lg font-mono text-black font-bold tracking-widest">
+              coffee and kicks
+            </p>
+            <div className="flex mt-[0.1rem]">
+            <p className="grow self-center text-xs font-mono tracking-widest">
+              caffeine near me
+            </p>
+            <p className="mr-2 scale-75"><FaTiktok /></p>
+            <p className="scale-75"><FaInstagram /></p>
+            </div>
           </div>
-        </div>
       </div>
-      </div>
+
+    </div>
 
     </div>
   );
